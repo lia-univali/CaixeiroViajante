a892e57c6920f3034a4232491a91234e968302b6
Vinícius Almeida dos Santos <vinicius@rudinei.cnt.br>
2017-09-30 16:31:33 -0300

    Added iterated search with initial constructed solution

--- setupalgorithms.h
+++ setupalgorithms.h
@@ -58,15 +58,43 @@ void setupAlgorithms(std::vector<ToolBox::AlgorithmData> &algorithms){
                 std::function<void()> onFinish
             ) -> void {
             Solution s = iteratedSearch(
-                        g,    // coordinates
-                        2000, // n iterations
-                        g.size()*0.2,    // num. of disturbance
+                        g,            // coordinates
+                        2000,         // n iterations
+                        g.size()*0.2, // num. of disturbance
                         setSolution, log, chartLog, stopRequested
             );
             setSolution( s );
             onFinish();
         };
         algorithms.emplace_back(iterated);
+    }
+    { /// ITERATED WITH CONSTRUCTIVE
+        ToolBox::AlgorithmData iteratedWithConstructive;
+        iteratedWithConstructive.name = "Busca Iterada com rota inicial";
+        iteratedWithConstructive.run = [](
+                GraphicData &g,
+                int initialNode,
+                std::function<void(const Solution&)> setSolution,
+                std::function<void(std::string)> log,
+                std::function<void(double)> chartLog,
+                std::function<bool()> stopRequested,
+                std::function<void()> onFinish
+            ) -> void {
+            Solution s = minimumLocalRoute(g, initialNode, setSolution, log, chartLog, stopRequested);
+            setSolution( s );
+            if ( !stopRequested() ){
+                s = iteratedSearch(
+                            g,            // coordinates
+                            s.path,       // start path
+                            2000,         // n iterations
+                            g.size()*0.2, // num. of disturbance
+                            setSolution, log, chartLog, stopRequested
+                );
+                setSolution( s );
+            }
+            onFinish();
+        };
+        algorithms.emplace_back(iteratedWithConstructive);
     }
 
 }
